601ff5b581e86e9cdc843054322653c934374a18
Martin <martinpastorino013@gmail.com>
2022-08-22 19:52:49 -0300

    make it better

--- src/main/java/com/portfolio/mp/Entity/Skill.java
+++ src/main/java/com/portfolio/mp/Entity/Skill.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfolio.mp.Entity;
 
 import javax.persistence.Entity;
@@ -9,8 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import static jdk.internal.joptsimple.internal.Messages.message;
+
 
 /**
  *
